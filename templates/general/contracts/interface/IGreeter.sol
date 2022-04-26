3e1d9a559a95118c8d659025964da89d951347a3
Jerrywang959 <59097122+Jerrywang959@users.noreply.github.com>
2022-04-26 20:52:53 +0800

    Fix: eslint doesnt work (#13)
    * 🐞 fix: lint cmd
    
    * 🌈 style: prettier

--- templates/general/contracts/interface/IGreeter.sol
+++ templates/general/contracts/interface/IGreeter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IGreeter {
-    function greet() external returns (string memory);
+  function greet() external returns (string memory);
 
-    function setGreeting(string memory _greeting) external;
+  function setGreeting(string memory _greeting) external;
 }
